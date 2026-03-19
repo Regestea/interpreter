6e2ee730405354c38d435213611867480f9ee187
regestea23 <amirhossein5starstar@outlook.com>
2026-03-19 23:59:57 +0330

    add font awesome

--- interpreter-web/src/features/Home.tsx
+++ interpreter-web/src/features/Home.tsx
@@ -1,10 +1,14 @@
 ﻿import {Link} from "react-router";
+import { faGithub, faReact } from '@fortawesome/free-brands-svg-icons'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 
 function Home() {
     return <>
             <p>homepage</p>
             <Link to="Settings"> to settings</Link>
+        <FontAwesomeIcon icon={faGithub} />
+        <FontAwesomeIcon icon={faReact} style={{ color: '#61dafb', animation: 'spin 2s linear infinite' }} />
     </>;
 }
 
