f76a3b39f65a25730876d6b957763ca5f2520003
regestea23 <amirhossein5starstar@outlook.com>
2026-03-20 11:07:03 +0330

    fix issue

--- interpreter-web/src/App.tsx
+++ interpreter-web/src/App.tsx
@@ -1,6 +1,5 @@
 import './App.css'
 import {Outlet} from "react-router";
-import {} from "@fortawesome/fontawesome-svg-core";
 import {faBookOpen,
     faEarthAsia,
     faRobot} from "@fortawesome/free-solid-svg-icons";
