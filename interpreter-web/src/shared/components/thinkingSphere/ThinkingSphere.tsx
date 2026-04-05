43e1fc2d3809ee2152d0e3d23620f4c572fbf8a6
regestea23 <amirhossein5starstar@outlook.com>
2026-04-05 10:27:49 +0330

    add thinking sphere animation

---
+++ interpreter-web/src/shared/components/thinkingSphere/ThinkingSphere.tsx
@@ -0,0 +1,14 @@
+﻿import styles from './ThinkingSphere.module.css';
+
+function ThinkingSphere() {
+    return (
+        <div className={styles.sphereLoader}>
+            <div className={`${styles.orb} ${styles.orb1}`}></div>
+            <div className={`${styles.orb} ${styles.orb2}`}></div>
+            <div className={`${styles.orb} ${styles.orb3}`}></div>
+            <div className={styles.glassLayer}></div>
+        </div>
+    );
+}
+
+export default ThinkingSphere;
